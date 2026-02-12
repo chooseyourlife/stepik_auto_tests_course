c5cac700ccc3e431b16df45b08921b4e21a33c32
choosetheone <brodvey77@yandex.ru>
2026-02-12 10:57:57 +0300

    update

--- section_3/3_2_8.py
+++ section_3/3_2_8.py
@@ -1,12 +1,36 @@
-s = 'My Name is Julia'
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
-if 'Name' in s:
-    print('Substring found')
+try:
+    link = "http://suninjuly.github.io/registration1.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
 
-index = s.find('Name')
-if index != -1:
-    print(f'Substring found at index {index}')
+    input1 = browser.find_element(By.XPATH, '//input[@placeholder="Input your first name"]')
+    input1.send_keys("Ivan")
+    input2 = browser.find_element(By.XPATH, '//input[@placeholder="Input your last name"]')
+    input2.send_keys("Petrov")
+    input3 = browser.find_element(By.CLASS_NAME, 'form-control.third')
+    input3.send_keys("brodvey77@gmail.com")
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
 
+    time.sleep(1)
+
+    # находим элемент, содержащий текст
+    welcome_text = browser.find_element(By.TAG_NAME, 'h1').text
+
+    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(5)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
 
 
 
---
+++ section_3/test_3_2_13.py
@@ -0,0 +1,73 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+class TestRegistrationLink(unittest.TestCase):
+
+    def test_registration(self):
+        try:
+            link = "http://suninjuly.github.io/registration1.html"
+            browser = webdriver.Chrome()
+            browser.get(link)
+
+            input1 = browser.find_element(By.XPATH, '//input[@placeholder="Input your first name"]')
+            input1.send_keys("Ivan")
+            input2 = browser.find_element(By.XPATH, '//input[@placeholder="Input your last name"]')
+            input2.send_keys("Petrov")
+            input3 = browser.find_element(By.CLASS_NAME, 'form-control.third')
+            input3.send_keys("brodvey77@gmail.com")
+            button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+            button.click()
+
+            time.sleep(0.5)
+
+            # находим элемент, содержащий текст
+            welcome_text = browser.find_element(By.TAG_NAME, 'h1').text
+
+            # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+            self.assertEqual("Congratulations! You have successfully registered!", welcome_text)
+
+        finally:
+            # успеваем скопировать код за 30 секунд
+            time.sleep(1)
+            # закрываем браузер после всех манипуляций
+            browser.quit()
+
+    def test_registration2(self):
+        try:
+            link = "http://suninjuly.github.io/registration2.html"
+            browser = webdriver.Chrome()
+            browser.get(link)
+
+            input1 = browser.find_element(By.XPATH, '//input[@placeholder="Input your first name"]')
+            input1.send_keys("Ivan")
+            input2 = browser.find_element(By.XPATH, '//input[@placeholder="Input your last name"]')
+            input2.send_keys("Petrov")
+            input3 = browser.find_element(By.CLASS_NAME, 'form-control.third')
+            input3.send_keys("brodvey77@gmail.com")
+            button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+            button.click()
+
+            time.sleep(0.5)
+
+            # находим элемент, содержащий текст
+            welcome_text = browser.find_element(By.TAG_NAME, 'h1').text
+
+            # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+            self.assertEqual("Congratulations! You have successfully registered!", welcome_text)
+
+        finally:
+            # успеваем скопировать код за 30 секунд
+            time.sleep(1)
+            # закрываем браузер после всех манипуляций
+            browser.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+
+
