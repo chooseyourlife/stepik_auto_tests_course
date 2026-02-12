239cf33ff96dff770f77d76f12b09c982c091649
choosetheone <brodvey77@yandex.ru>
2026-02-12 15:05:49 +0300

    update

--- section_3/test_fixture5.py
+++ section_3/test_fixture5.py
@@ -4,30 +4,27 @@ from selenium.webdriver.common.by import By
 
 link = "http://selenium1py.pythonanywhere.com/"
 
-@pytest.fixture
+
+@pytest.fixture(scope="class")
 def browser():
     print("\nstart browser for test..")
     browser = webdriver.Chrome()
     yield browser
-    # этот код выполнится после завершения теста
     print("\nquit browser..")
     browser.quit()
 
 
-@pytest.fixture(autouse=True)
-def prepare_data():
-    print()
-    print("preparing some critical data for every test")
-
-
 class TestMainPage1():
-    # вызываем фикстуру в тесте, передав ее как параметр
+
+    # вызываем фикстуру в тесте, передав ее как параметр
     def test_guest_should_see_login_link(self, browser):
-        # не передаём как параметр фикстуру prepare_data, 
-        # но она все равно выполняется
+        print("start test1")
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, "#login_link")
+        print("finish test1")
 
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        print("start test2")
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
+        print("finish test2")
